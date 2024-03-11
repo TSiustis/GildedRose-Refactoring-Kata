317494e00f6678b2e1f268826d8dc5d45cae8b71
tsioustis <144914296+tsioustis@users.noreply.github.com>
2024-03-11 21:41:39 +0100

    Added Factory.

---
+++ csharp/ItemStrategyFactory.cs
@@ -0,0 +1,14 @@
+﻿namespace csharp;
+public static class ItemStrategyFactory
+{
+    public static ItemStrategy GetStrategy(Item item)
+    {
+        return item.Name switch
+        {
+            string agedBrie when agedBrie.Contains("Aged Brie") => new AgedBrieStrategy(item),
+            string backstagePass when backstagePass.Contains("Backstage pass") => new BackstagePassStrategy(item),
+            string sulfuras when sulfuras.Contains("Sulfuras") => new SulfurasStrategy(item),
+            _ => new NormalItemStrategy(item),
+        };
+    }
+}
